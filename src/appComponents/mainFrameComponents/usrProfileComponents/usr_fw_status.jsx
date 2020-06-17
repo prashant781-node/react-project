41d16f0245d0c213bb6e2349b5bd7a804e7ea316
prashant781-node <kansal7.81@gmail.com>
2020-06-18 04:41:46 +0530

    minor change

--- src/appComponents/mainFrameComponents/usrProfileComponents/usr_fw_status.jsx
+++ src/appComponents/mainFrameComponents/usrProfileComponents/usr_fw_status.jsx
@@ -1,13 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 class UsrFwStatus extends Component {
-    state = {}
-    render() {
-        return (<ul className="user-fw-status">
-            <li><h4>Friends <span className="puli-right">{this.props.user.friends.length}</span></h4></li>
-            <li><h4>Nominee <span className="puli-right">{this.props.user.nominee.length}</span></h4></li>
-            <li><a href="http://goinstablog.com/goinstablog.com/sumitdesign/design/liveon.com/" title="">View Profile</a></li>
-        </ul>);
-    }
+  state = {};
+  render() {
+    return (
+      <ul className="user-fw-status">
+        <li>
+          <h4>
+            Friends{" "}
+            <span className="puli-right">{this.props.user.friends.length}</span>
+          </h4>
+        </li>
+        <li>
+          <h4>
+            Nominee{" "}
+            <span className="puli-right">{this.props.user.nominee.length}</span>
+          </h4>
+        </li>
+        <li>
+          <a href="/src/index.js" title="">
+            View Profile
+          </a>
+        </li>
+      </ul>
+    );
+  }
 }
 
 export default UsrFwStatus;
